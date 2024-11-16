7b1235595ba9830f6d0cd63967c05605d0901c94
Gabriel De Souza Almeida <gabrielsouzaalmeida07@gmail.com>
2024-11-16 14:03:35 -0300

    Post API Auth: Ajuste erro 500: AutoMapper.AutoMapperMappingException: Missing type map configuration or unsupported mapping.

---
+++ template/backend/src/Ambev.DeveloperEvaluation.WebApi/Mappings/MappingProfile.cs
@@ -0,0 +1,37 @@
+﻿using Ambev.DeveloperEvaluation.Application.Auth.AuthenticateUser;
+using Ambev.DeveloperEvaluation.Application.Users.GetUser;
+using Ambev.DeveloperEvaluation.WebApi.Features.Auth.AuthenticateUserFeature;
+using Ambev.DeveloperEvaluation.WebApi.Features.Users.GetUser;
+using AutoMapper;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // Mapeando de GetUserResult para GetUserResponse
+            CreateMap<GetUserResult, GetUserResponse>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
+                .ForMember(dest => dest.Phone, opt => opt.MapFrom(src => src.Phone))
+                .ForMember(dest => dest.Role, opt => opt.MapFrom(src => src.Role))
+                .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status));
+
+
+            // Mapeando de GetUserResult para GetUserResponse
+            CreateMap<AuthenticateUserRequest, AuthenticateUserCommand>()
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
+                .ForMember(dest => dest.Password, opt => opt.MapFrom(src => src.Password));
+
+            // Mapeando de GetUserResult para GetUserResponse
+            CreateMap<AuthenticateUserResult, AuthenticateUserResponse>()
+                .ForMember(dest => dest.Token, opt => opt.MapFrom(src => src.Email))
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.Role, opt => opt.MapFrom(src => src.Role));
+        }
+
+    }
+}

708666c94b2bd1d31acf961cc6de791e7f658524
Gabriel De Souza Almeida <gabrielsouzaalmeida07@gmail.com>
2024-11-16 01:27:34 -0300

    Ajuste erro 500: AutoMapper.AutoMapperMappingException: Missing type map configuration or unsupported mapping.

---
+++ template/backend/src/Ambev.DeveloperEvaluation.WebApi/Features/Users/GetUser/MappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using Ambev.DeveloperEvaluation.Application.Users.GetUser;
+using AutoMapper;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Features.Users.GetUser
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // Mapeando de GetUserResult para GetUserResponse
+            CreateMap<GetUserResult, GetUserResponse>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
+                .ForMember(dest => dest.Phone, opt => opt.MapFrom(src => src.Phone))
+                .ForMember(dest => dest.Role, opt => opt.MapFrom(src => src.Role))
+                .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status));
+        }
+
+    }
+}
